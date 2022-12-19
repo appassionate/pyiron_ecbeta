75a6a6ab24339e5e10f3a7acb1eb88d5a71ac44c
Jan Janssen <jan-janssen@users.noreply.github.com>
2022-12-19 10:28:42 -0700

    Load classes in the JOB_CLASS_DICT

--- pyiron_echem/__init__.py
+++ pyiron_echem/__init__.py
@@ -1,1 +1,8 @@
 __version__ = "0.0.1"
+
+
+from pyiron_base import JOB_CLASS_DICT
+
+# Make classes available for new pyiron version
+JOB_CLASS_DICT['Cp2kJob'] = 'pyiron_ecbeta.cp2k.cp2k'
+JOB_CLASS_DICT['Vasplite'] = 'pyiron_ecbeta.vasplite.vasplite'
